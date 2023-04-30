237a684b96be5b52c77e5ac4ed8ad6e2968d7e6c
FadyEskander6424 <130233728+FadyEskander6424@users.noreply.github.com>
2023-04-30 23:24:39 +0200

    Create assign_04_problem_01_code.cpp

---
+++ assign_04_problem_01_code.cpp
@@ -0,0 +1,24 @@
+//fady hany phillip
+//20216424
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number of numbers");
+        int x = sc.nextInt();
+        int[] num = new int[x];
+        for (int y = 0; y <= x; y++) {
+            num[y] = sc.nextInt();
+            if (num[y] >= 1) {
+                System.out.print("1"+" ");
+            } else if (num[y] == 0) {
+                System.out.print("0"+" ");
+            } else if (num[y] < 0) {
+                System.out.print(Math.abs(num[y])+" ");
+
+            }
+        }
+    }
+}
