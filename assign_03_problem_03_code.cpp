cfe7c42d21628827295b688e62738be355bfefce
FadyEskander6424 <130233728+FadyEskander6424@users.noreply.github.com>
2023-04-30 21:55:52 +0200

    Create assign_03_problem_03_code.cpp

---
+++ assign_03_problem_03_code.cpp
@@ -0,0 +1,24 @@
+//fady hany phillip
+//20216424
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Pum");
+            } else if (i % 3 == 0) {
+                System.out.println("Pum");
+            } else if (i % 5 == 0) {
+                System.out.println("Pum");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
