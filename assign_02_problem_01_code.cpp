f49a9b933b3e31a1bcdbe5081f0e0e973bc4b4e9
FadyEskander6424 <130233728+FadyEskander6424@users.noreply.github.com>
2023-04-30 21:49:26 +0200

    Create assign_02_problem_01_code.cpp

---
+++ assign_02_problem_01_code.cpp
@@ -0,0 +1,12 @@
+//fady hany phillip
+//20216424
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int x = 1; x <= num; x++) {
+            System.out.println(x);
+        }
+    }
+}
